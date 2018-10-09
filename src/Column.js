e9671061810fcc45c5c6b93ab9bb5ea884f457c7
Amir PC <amirhasankntu@gmail.com>
2018-10-08 23:09:12 -0400

    final push

---
+++ src/Column.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Column = props => <div className='card-item'> {props.children} </div>
+
+
+export default Column;
---
+++ src/Row.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Row = props =>
+  <div className="grid-row">
+    {props.children}
+  </div>;
+
+export default Row;
